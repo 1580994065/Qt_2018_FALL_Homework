10b6add561766052937b9243ed00df64bc334443
liu <1731654422@qq.com>
2018-09-11 21:49:35 +0800

    Revert "Revert "functions is alright""
    This reverts commit 624e8a4c482f2540166a4cb48fab4fb9852d8402.

---
+++ untitled/main.cpp
@@ -0,0 +1,56 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <array>
+#include <QList>
+#include<algorithm>
+
+using namespace std;
+
+#define  WORD_HI(xxx)  (((xxx) >> 24))         //取32位最高8位
+#define  WORD_LHI(xxx)  (((xxx) >> 16)& 255)   //取32位次高8位
+#define  WORD_HLO(xxx)  ((((xxx) >>8) & 255))  //取32位次低8位
+#define  WORD_LO(xxx)  (((xxx) & 255))         //取32位最低8位
+
+#define  MAX(x, y)  ( ((x) > (y)) ? (x) : (y) )//取最大值
+#define  MIN(x, y)  ( ((x) < (y)) ? (x) : (y) )//取最小值
+
+#define  at(x) values.at(x)
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    unsigned int test=0x12345678;               //测试数据
+    QList<qint8> values;
+
+    //向数组中添加各位数
+    values<<WORD_HI(test);
+    values<<WORD_LHI(test);
+    values<<WORD_HLO(test);
+    values<<WORD_LO(test);
+    qDebug("原始值:0x%x==%d",test,test);       //显示原始数值
+    qDebug("0x%x==%d 0x%x==%d 0x%x==%d 0x%x==%d",//显示各8位数值
+           values.at(0),values.at(0),values.at(1),values.at(1),
+           values.at(2),values.at(2),values.at(3),values.at(3));
+
+    //比较各位数据
+    qDebug("最高8位和次高8位最大值：0x%x(%d)"       //比较高位大小
+           ,MAX(values.at(0),values.at(1)),MAX(values.at(0),values.at(1)));
+    qDebug("最低8位和次低8位最大值：0x%x(%d)"       //比较低位大小
+           ,MIN(values.at(2),values.at(3)),MAX(values.at(2),values.at(3)));
+
+    //重新组合数据
+    unsigned int new_test=values.at(1)
+                          +(values.at(3)<<8)
+                          +(values.at(0)<<16)
+                          +(values.at(2)<<24);
+    qDebug("重新组合后数值:0x%x(%d)",new_test,new_test);
+
+    //排序数组
+    qDebug("排序前：（%x,%x,%x,%x）"               //显示原序列
+           ,values.at(0),values.at(1),values.at(2),values.at(3));
+    sort(values.rbegin(),values.rend(),qLess<qint8>());
+    qDebug("排序后：（%x,%x,%x,%x）"               //显示排序后序列
+           ,values.at(0),values.at(1),values.at(2),values.at(3));
+    return a.exec();
+}
