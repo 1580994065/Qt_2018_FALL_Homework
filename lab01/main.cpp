9ef9f4837337065fad560ef9500c74b0b7136464
liu <1731654422@qq.com>
2018-10-12 22:37:08 +0800

    use functions based on qt instead of std

--- lab01/main.cpp
+++ lab01/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <QCoreApplication>
+#include <QVector>
 #include<QDebug>
 
 using namespace std;
@@ -10,14 +11,69 @@ using namespace std;
  *problems solving: the compatibility of data output is a big problem
  *                  firstly I want to use qdebug to output qstring,unsolved
  *                  then cout,text in chaos,change code to GBK can solve this
- *                  but qdebug still can't. */
+ *                  but qdebug still can't.
+ *                  finaly i figure out that encode file with UTF-8 can output
+ *                  with qdebug.
+*/
+
 
 typedef struct{
-    string stu_num="";
-    string name="";
-    int subject1;
-    int subject2;
+    QString stu_num="";
+    QString name="";
+    qint64 subject1;
+    qint64 subject2;
 }stu_data;
+
+void display(QVector<stu_data> *data);
+bool cmp_name(stu_data a,stu_data b);
+bool cmp_subject1(stu_data a,stu_data b);
+bool cmp_subject2(stu_data a,stu_data b);
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    //form test datas
+    stu_data data_1,data_2,data_3,data_4;
+    //stu 1
+    data_1.stu_num="1403130209";data_1.name="é²æ™ºæ·±";data_1.subject1=80;data_1.subject2=72;
+    //stu 2
+    data_2.stu_num="1403140101";data_2.name="æ—å†²";data_2.subject1=82;data_2.subject2=76;
+    //stu 3
+    data_3.stu_num="1403140102";data_3.name="å®‹æ±Ÿ";data_3.subject1=76;data_3.subject2=85;
+    //stu4
+    data_4.stu_num="1403140103";data_4.name="æ­¦æ¾";data_4.subject1=88;data_4.subject2=80;
+
+    //using vector to counect 4 struct
+    QVector<stu_data> data;
+    data.push_back(data_1);
+    data.push_back(data_2);
+    data.push_back(data_3);
+    data.push_back(data_4);
+
+    //display orginal data
+    qDebug()<<"åŸæ•°æ®:\n";
+    display(&data);
+
+    //sort of name
+    QVector<stu_data> sort_name=data;
+    sort(sort_name.begin(),sort_name.end(),cmp_name);
+    qDebug()<<"æŒ‰å§“åæ’åº:\n";
+    display(&sort_name);
+
+    //sort of subject1
+    QVector<stu_data> sort_subject1=data;
+    sort(sort_subject1.begin(),sort_subject1.end(),cmp_subject1);
+    qDebug()<<"æŒ‰è¯¾ç¨‹ä¸€æ’åºï¼š\n";
+    display(&sort_subject1);
+
+    //sort of subject2
+    QVector<stu_data> sort_subject2=data;
+    sort(sort_subject2.begin(),sort_subject2.end(),cmp_subject2);
+    qDebug()<<"æŒ‰è¯¾ç¨‹äºŒæ’åºï¼š\n";
+    display(&sort_subject2);
+
+    return a.exec();
+}
 
 /**
  * @brief display
@@ -25,12 +81,12 @@ typedef struct{
  * @param data
  * pointer of struct data
  */
-void display(vector<stu_data> *data)
+void display(QVector<stu_data> *data)
 {
-    cout<<"Ñ§ºÅ"<<"           "<<"ĞÕÃû"<<"     "<<"¿Î³Ì1"<<"  "<<"¿Î³Ì2"<<endl;
-    for(vector<stu_data>::iterator m = data->begin(); m != data->end(); m++ )//m is temp struct
+    qDebug()<<"å­¦å·"<<"             "<<"å§“å"<<"       "<<"è¯¾ç¨‹1"<<"  "<<"è¯¾ç¨‹2\n";
+    for(QVector<stu_data>::iterator m = data->begin(); m != data->end(); m++ )//m is temp struct
     {
-        cout<<m->stu_num<<"     "<<m->name<<"     "<<m->subject1<<"     "<<m->subject2<<endl;
+        qDebug()<<m->stu_num<<"     "<<m->name<<"     "<<m->subject1<<"     "<<m->subject2<<"\n";
         //printf("%s    %s    %d     %d\n",m->stu_num,m->name,m->subject1,m->subject2);
     }
     cout<<endl;
@@ -83,70 +139,13 @@ bool cmp_subject1(stu_data a,stu_data b)
  */
 bool cmp_subject2(stu_data a,stu_data b)
 {
-    if(a.subject1>=b.subject1)
-    {
-        return 0;
-    }
-    else
+    if(a.subject2>=b.subject2)
     {
         return 1;
     }
+    else
+    {
+        return 0;
+    }
 }
-
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-    //form test datas
-    stu_data data_1,data_2,data_3,data_4;
-    //stu 1
-    data_1.stu_num="1403130209";
-    data_1.name="Â³ÖÇÉî";
-    data_1.subject1=80;
-    data_1.subject2=72;
-    //stu 2
-    data_2.stu_num="1403140101";
-    data_2.name="ÁÖ³å";
-    data_2.subject1=82;
-    data_2.subject2=76;
-    //stu 3
-    data_3.stu_num="1403140102";
-    data_3.name="ËÎ½­";
-    data_3.subject1=76;
-    data_3.subject2=85;
-    //stu4
-    data_4.stu_num="1403140103";
-    data_4.name="ÎäËÉ";
-    data_4.subject1=88;
-    data_4.subject2=80;
-    //using vector to counect 4 struct
-    vector<stu_data> data;
-    data.push_back(data_1);
-    data.push_back(data_2);
-    data.push_back(data_3);
-    data.push_back(data_4);
-
-    //display orginal data
-    cout<<"Ô­Êı¾İ:\n";
-    display(&data);
-
-    //sort of name
-    vector<stu_data> sort_name=data;
-    sort(sort_name.begin(),sort_name.end(),cmp_name);
-    cout<<"°´ĞÕÃûÅÅĞò:"<<endl;
-    display(&sort_name);
-
-    //sort of subject1
-    vector<stu_data> sort_subject1=data;
-    sort(sort_subject1.begin(),sort_subject1.end(),cmp_subject1);
-    cout<<"°´¿Î³ÌÒ»ÅÅĞò£º"<<endl;
-    display(&sort_subject1);
-
-    //sort of subject2
-    vector<stu_data> sort_subject2=data;
-    sort(sort_subject2.begin(),sort_subject2.end(),cmp_subject2);
-    cout<<"°´¿Î³Ì¶şÅÅĞò£º"<<endl;
-    display(&sort_subject2);
-    return a.exec();
-}
-
 
